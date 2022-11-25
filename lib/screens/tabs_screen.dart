53b7e155ddded673561b304298b4ad9a0e8cd1ef
Rawa-Osman <rawaosman134@gmail.com>
2022-11-25 19:49:26 +0300

    completed

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/main_drawer.dart';
+import './favorites_screen.dart';
+import './categories_screen.dart';
+import '../models/meal.dart';
+
+class TabsScreen extends StatefulWidget {
+  final List<Meal> favoriteMeals;
+
+  TabsScreen(this.favoriteMeals);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  List<Map<String, Object>> _pages;
+  int _selectedPageIndex = 0;
+
+  @override
+  void initState() {
+    _pages = [
+      {
+        'page': CategoriesScreen(),
+        'title': 'Categories',
+      },
+      {
+        'page': FavoritesScreen(widget.favoriteMeals),
+        'title': 'Your Favorite',
+      },
+    ];
+    super.initState();
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedPageIndex,
+        // type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            label: ('Categories'),
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            label: ('Favorites'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// import 'package:flutter/material.dart';
+
+// import './favorites_screen.dart';
+// import './Categories_screen.dart';
+
+// class TabsScreen extends StatefulWidget {
+//   const TabsScreen({Key key}) : super(key: key);
+
+//   @override
+//   State<TabsScreen> createState() => _TabsScreenState();
+// }
+
+// class _TabsScreenState extends State<TabsScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       initialIndex: 0,
+//       length: 2,
+//       child: Scaffold(
+//         backgroundColor: Theme.of(context).canvasColor,
+//         appBar: AppBar(
+//           title: Text('meals'),
+//         ),
+//         bottomNavigationBar: TabBar(
+//           labelColor: Colors.amber,
+//           tabs: [
+//             Tab(
+//               icon: Icon(
+//                 Icons.category,
+//               ),
+//               text: 'Categories',
+//             ),
+//             Tab(
+//               icon: Icon(Icons.star),
+//               text: 'favorites',
+//             ),
+//           ],
+//         ),
+//         body: TabBarView(
+//           children: [
+//             CategoriesScreen(),
+//             FavoritesScreen(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
